d7bffb6452273749d95b1190bba4db6740b7f2d5
Quân Ninh <ninhquan391@gmail.com>
2024-11-26 14:23:56 +0700

    sửa lần 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx làm đến đây rồ
+              src/app/page.tsx sửa lần 1s
             </code>
             .
           </li>
